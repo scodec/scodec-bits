52b65d493583441a430c3dec3f36f950f14684f5
Michael Pilquist <mpilquist@gmail.com>
2020-01-27 21:45:54 -0500

    Fixed build and reduced some duplication in FromDigits support

--- core/shared/src/main/scala-0.22/scodec/bits/ByteVectorPlatform.scala
+++ core/shared/src/main/scala-0.22/scodec/bits/ByteVectorPlatform.scala
@@ -4,24 +4,30 @@ import scala.quoted._
 import scala.quoted.matching._
 import scala.util.FromDigits
 
-trait ByteVectorPlatform {
+// TODO: as of 0.22, making this private[bits] results in errors like the following at call sites:
+// scala> val b: ByteVector = 0x00112233_44556677_8899aabb_ccddeeff_fedba098
+// trait ByteVectorPlatform cannot be accessed as a member of scodec.bits from module class rs$line$2$.
+private[bits] trait ByteVectorPlatform { self: ByteVector.type =>
 
   class FromDigits extends FromDigits.WithRadix[ByteVector] {
     def fromDigits(digits: String, radix: Int): ByteVector =
-      if (radix == 16) ByteVector.fromValidHex(digits.tail)
-      else throw FromDigits.MalformedNumber("unsupported radix")
+      digitsToByteVector(digits, radix)
   }
 
   given FromDigits {
     override inline def fromDigits(digits: String): ByteVector =
-      ${digitsToByteVector('digits, Expr(10))}
+      ${digitsToByteVectorMacro('digits, Expr(10))}
     override inline def fromDigits(digits: String, radix: Int): ByteVector =
-      ${digitsToByteVector('digits, 'radix)}
+      ${digitsToByteVectorMacro('digits, 'radix)}
   }
 }
 
+private[bits] def digitsToByteVector(digits: String, radix: Int): ByteVector =
+  if (radix == 16) ByteVector.fromValidHex(digits.tail)
+  else throw FromDigits.MalformedNumber(s"unsupported radix $radix")
 
-def digitsToByteVector(digits: Expr[String], radix: Expr[Int])(given qctx: QuoteContext): Expr[ByteVector] =
+
+def digitsToByteVectorMacro(digits: Expr[String], radix: Expr[Int])(given qctx: QuoteContext): Expr[ByteVector] =
   (digits, radix) match {
     case (Const(ds), Const(r)) =>
       if (r == 16) {
@@ -31,8 +37,5 @@ def digitsToByteVector(digits: Expr[String], radix: Expr[Int])(given qctx: Quote
         '{ByteVector.empty}
       }
     case other =>
-      '{
-          if ($radix == 16) ByteVector.fromValidHex($digits.tail)
-          else throw FromDigits.MalformedNumber("unsupported radix")
-      }
+      '{digitsToByteVector($digits, $radix)}
   }
