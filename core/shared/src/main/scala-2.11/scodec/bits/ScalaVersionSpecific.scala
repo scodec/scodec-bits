079f5f454d0828d78862ad5e0d1d9361fad487bd
Eric Peters <eric@peters.org>
2020-06-17 10:19:23 -0700

    Add Scala 2.11.x support

---
+++ core/shared/src/main/scala-2.11/scodec/bits/ScalaVersionSpecific.scala
@@ -0,0 +1,9 @@
+package scodec.bits
+
+private[bits] trait ScalaVersionSpecific {
+  type IterableOnce[+A] = collection.GenTraversableOnce[A]
+
+  implicit class IterableOnceOps[A](private val self: IterableOnce[A]) {
+    def iterator: Iterator[A] = self.toIterator
+  }
+}
