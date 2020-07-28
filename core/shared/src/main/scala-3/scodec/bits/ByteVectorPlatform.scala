c0fc5c11b205dc037dcc1a81c819c93cb291be9c
mpilquist <mpilquist@gmail.com>
2020-07-27 21:57:22 -0400

    Undo scalafmt dotty changes

--- core/shared/src/main/scala-3/scodec/bits/ByteVectorPlatform.scala
+++ core/shared/src/main/scala-3/scodec/bits/ByteVectorPlatform.scala
@@ -3,11 +3,11 @@ package scodec.bits
 import scala.util.FromDigits
 
 private[bits] trait ByteVectorPlatform { self: ByteVector.type =>
-  given.as(FromDigits.WithRadix[ByteVector] {
+  given as FromDigits.WithRadix[ByteVector] {
     def fromDigits(digits: String, radix: Int): ByteVector =
       radix match {
         case 16 => ByteVector.fromValidHex(digits)
         case _  => ByteVector.fromValidHex(new java.math.BigInteger(digits, radix).toString(16))
       }
-  })
+  }
 }
