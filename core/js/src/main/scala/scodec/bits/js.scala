221a20407b2f0402c7f61fda44c899f53ae60086
Pierre Kisters <pierrekisters@gmail.com>
2021-04-06 13:23:23 +0200

    js Int8Array and ArrayBuffer interop

---
+++ core/js/src/main/scala/scodec/bits/js.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2013, Scodec
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package scodec.bits
+
+import scala.scalajs.js.typedarray.{ArrayBuffer, Int8Array, TypedArrayBuffer}
+
+object js {
+
+  implicit class ByteVectorJSOps(val self: ByteVector) extends AnyVal {
+    def copyToTypedArray(dest: Int8Array, start: Int): Unit = {
+      val len: Int = self.intSize.getOrElse(throw new RuntimeException("ByteVector too large!"))
+      self.copyToTypedArray(dest, start, 0, len)
+    }
+
+    def copyToTypedArray(dest: Int8Array, start: Int, offset: Long, size: Int): Unit = {
+      var i = 0
+      while (i < size) {
+        dest(start + i) = self(offset + i)
+        i += 1
+      }
+    }
+
+    def toTypedArray: Int8Array = {
+      val len = self.intSize.getOrElse(throw new RuntimeException("ByteVector too large!"))
+      val dest = new Int8Array(len)
+      self.copyToTypedArray(dest, 0, 0, len)
+      dest
+    }
+  }
+
+  implicit class ByteVectorObjJSOps(val self: ByteVector.type) extends AnyVal {
+    def view(typedArray: Int8Array): ByteVector = ByteVector.view(TypedArrayBuffer.wrap(typedArray))
+
+    def view(arrayBuffer: ArrayBuffer): ByteVector = ByteVector.view(TypedArrayBuffer.wrap(arrayBuffer))
+
+    def fromTypedArray(typedArray: Int8Array): ByteVector = {
+      val copy = new Int8Array(typedArray.length)
+      copy.set(typedArray)
+      ByteVector.view(copy)
+    }
+
+    def fromArrayBuffer(arrayBuffer: ArrayBuffer): ByteVector = ByteVector.fromTypedArray(new Int8Array(arrayBuffer))
+  }
+
+}
