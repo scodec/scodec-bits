53b0ce416f652d8795f30bb837c47d6939e3d9fe
Scala Steward <me@scala-steward.org>
2021-09-25 20:21:04 +0200

    Reformat with scalafmt 3.0.5

--- core/js/src/main/scala/scodec/bits/ByteVectorCompanionCrossPlatform.scala
+++ core/js/src/main/scala/scodec/bits/ByteVectorCompanionCrossPlatform.scala
@@ -32,9 +32,12 @@ package scodec.bits
 
 import scala.scalajs.js.typedarray.{ArrayBuffer, TypedArrayBuffer, Uint8Array}
 
-private[bits] trait ByteVectorCompanionCrossPlatform extends ByteVectorPlatform { self: ByteVector.type =>
+private[bits] trait ByteVectorCompanionCrossPlatform extends ByteVectorPlatform {
+  self: ByteVector.type =>
   def view(typedArray: Uint8Array): ByteVector =
-    ByteVector.view(TypedArrayBuffer.wrap(typedArray.buffer, typedArray.byteOffset, typedArray.byteLength))
+    ByteVector.view(
+      TypedArrayBuffer.wrap(typedArray.buffer, typedArray.byteOffset, typedArray.byteLength)
+    )
 
   def view(arrayBuffer: ArrayBuffer): ByteVector =
     ByteVector.view(TypedArrayBuffer.wrap(arrayBuffer))
